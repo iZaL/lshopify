56d6629fb4f8088bb80fd00f32aada3779068ae3
Afzal Abbas <z4ls@live.com>
2022-02-12 07:34:28 +0530

    refactor options

---
+++ database/migrations/2021_07_10_172903_create_variant_options_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVariantOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variant_options', function (Blueprint $table) {
+            $table->id();
+            $table->integer('variant_id');
+            $table->integer('option_id');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variant_options');
+    }
+}
