7e9ec4e7446bda45ec78dc346c045b8635d0340d
Afzal Abbas <z4ls@live.com>
2022-03-07 13:22:51 +0400

    add smart table for collections

--- resources/js/Collection/components/CollectionList.tsx
+++ resources/js/Collection/components/CollectionList.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Collection} from '../../types';
+import { Collection, CollectionCondition } from '../../types'
 import {Inertia} from '@inertiajs/inertia';
 import route from 'ziggy-js';
 import Checkbox from '../../components/forms/Checkbox';
 import Button from '../../components/Button';
 import VariantImage from '../../Product/Variant/components/VariantImage';
 import Table from '../../components/Table';
+import SmartTable from '../../components/SmartTable'
 
 interface Props {
   collections: Collection[];
@@ -17,47 +18,55 @@ export default function CollectionList({collections}: Props) {
   };
 
   return (
-    <Table>
-      <thead>
-        <tr>
-          <Table.Head headerStyle="w-16" />
-          <Table.Head title="Title" />
-          <Table.Head title="Product conditions" />
-          <Table.Head />
-        </tr>
-      </thead>
-      <tbody>
-        {collections.map((collection, id) => (
-          <Table.Row key={id} idx={id}>
-            <Table.Col>
-              <div className="flex w-12 items-center justify-center">
-                <Checkbox checked={false} onChange={() => {}} name="" />
-              </div>
-            </Table.Col>
+    <>
+      <SmartTable items={collections}>
+        <SmartTable.SmartHeader>
+          {({selectedItemIDs}) => {
+            return (
+              <></>
+            )
+          }}
+        </SmartTable.SmartHeader>
 
-            <Table.Col>
-              <Button
-                theme="clear"
-                onClick={() => onCollectionClick(collection)}>
-                {collection.image && (
-                  <VariantImage
-                    onClick={() => onCollectionClick(collection)}
-                    image={collection.image}
-                    imageStyle="w-16 h-16 mr-2"
-                  />
-                )}
-                {collection.name}
-              </Button>
-            </Table.Col>
-            <Table.Col>{collection.type}</Table.Col>
-            <Table.Col>
-              {collection.conditions.map((condition, idx) => (
-                <div key={idx}>{condition.title}</div>
-              ))}
-            </Table.Col>
-          </Table.Row>
-        ))}
-      </tbody>
-    </Table>
+        <Table>
+          <SmartTable.Header>
+            <Table.Head headerStyle="w-16" />
+            <Table.Head title="Title" />
+            <Table.Head title="Product conditions" />
+            <Table.Head />
+          </SmartTable.Header>
+          <SmartTable.Body>
+
+            {({item}) => {
+              return (
+                <>
+                  <Table.Col>
+                    <Button
+                      theme="clear"
+                      onClick={() => onCollectionClick(item)}>
+                      {item.image && (
+                        <VariantImage
+                          onClick={() => onCollectionClick(item)}
+                          image={item.image}
+                          imageStyle="w-16 h-16 mr-2"
+                        />
+                      )}
+                      {item.name}
+                    </Button>
+                  </Table.Col>
+                  <Table.Col>{item.type}</Table.Col>
+                  <Table.Col>
+                    {item.conditions.map((condition:CollectionCondition, idx:number) => (
+                      <div key={idx}>{condition.title}</div>
+                    ))}
+                  </Table.Col>
+                </>
+              )
+            }}
+
+          </SmartTable.Body>
+        </Table>
+      </SmartTable>
+    </>
   );
 }
